520fd517b275e0be787ad7202c12ef55b4193c6c
Vitor-NLF <loureirovitornascimento@gmail.com>
2024-11-08 10:02:45 -0400

    atualizando...

--- titulo.js
+++ titulo.js
@@ -1,10 +1,10 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const titulo = document.querySelector('.titulo-box');
-    titulo.innerHTML = titulo.textContent.split('').map(letter => `<span>${letter}</span>`).join('');
-});
-
 
 document.addEventListener('DOMContentLoaded', () => {
-    const titulo = document.querySelector('.titulo-box-experiencias');
-    titulo.innerHTML = titulo.textContent.split('').map(letter => `<span>${letter}</span>`).join('');
+    const titulo2 = document.querySelector('.titulo-box-exp');
+    titulo2.innerHTML = titulo2.textContent.split('').map(letter => `<span>${letter}</span>`).join('');
 });
+document.addEventListener('DOMContentLoaded', () => {
+    const titulo3 = document.querySelector('.titulo-box-proj');
+    titulo3.innerHTML = titulo3.textContent.split('').map(letter => `<span>${letter}</span>`).join('');
+});
+
